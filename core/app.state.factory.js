f04ec1d2c6e92011124df372e1a91cede4f0635d
treythompsonphx <52680770+treythompsonphx@users.noreply.github.com>
2019-07-10 13:55:49 -0700

    updated contact inforamtion for each location
    added Indonesia, modified VM names for each country

--- core/app.state.factory.js
+++ core/app.state.factory.js
@@ -46,7 +46,7 @@
       /*   UNITED STATES   */
       /*********************/
       US: {
-        appName: 'MyMoney',
+        appName: 'MyMoney-US',
         label: 'US (English)',
         description: 'United States',
         code: 'US',
@@ -139,17 +139,17 @@
       /**********************/
 
       UK: {
-        appName: 'MySmackers',
+        appName: 'MyMoney-UK',
         label: 'UK (English)',
         description: 'United Kingdom',
         code: 'UK',
         ISOCode: 'GB',
         persons: [
           ['', '', '', '', '', '', '', '', '', ''],
-          ['Sarah', 'Jamison', '02/01/1976', 'AO 12 34 56', '17123 Peachtree Court', 'Edinburgh', 'Scotland', 'EH10 4BF', '+44 7911 123456', 'sarahlynnjamison0276@yahoo.com'],
-          ['Lizzy', 'Grant', '04/04/1983', 'MB 56 65 54 C', '25301 3rd Avenue APT 184', 'London', 'England', 'W1T 1JY', '+44 7911 123456', 'lizzy_grant@gmail.com'],
-          ['Elizabeth', 'Grant', '07/04/1983', 'MB 56 65 54 C', '7146 S Apostle Avenue #8160', 'Bath', 'England', 'BA1 2FJ', '+44 7911 123456', 'elizabethgrant83@yahoo.com'],
-          ['Jeremy', 'Vasquez', '09/18/1999', 'XL 55 33 45 C', '9715 Rock Maple Street', 'Brighton', 'East Sussex, England', 'BN1 2NW', '+44 7911 123456', 'jeremyv_007@gmail.com'],
+          ['Sarah','Jamison','02-01-1976','AO 12 34 56 A','23 High Tree Road','Nottingham', 'England','NG1 3SD','+44 6175 364100','sarahlynnjamison0276@yahoo.com'],
+		  ['Lizzy','Grant','09-04-1973','AO 12 34 56 B','96 Parkfield Road','Upper Healy','Derby','DE23 1GH','+44 3125 678500','lizzy_grant@gmail.com'],
+		  ['Elizabeth','Grant','09-04-1973','AO 12 34 56 B','42 West Wellington Drive','Wygston','Leicester','LE18 6PD', '+44 3125 678500','elizabethgrant83@yahoo.com'],
+		  ['Paul','Johnson','09-18-1999','OW 07 68 52D','104 Greyfriars Road','Cardiff','England','CF1 3YQ','+44 07 0517 11863','PaulA_007@gmail.com'],
         ],
         text: {
           chooseScenario: 'Choose scenario',
@@ -158,7 +158,7 @@
         },
         scenarios: {
           typical: {
-            label: 'Ordinary Applicant', title: 'Typical applicant: multiple services but no friction',
+            label: 'Typical Applicant', title: 'Typical applicant: multiple services but no friction',
             description: 'CrossCore strategies can be easily configured to conditionally include certain services based on risk while skipping others.',
             person: 1,
           },
@@ -194,7 +194,7 @@
           },
           {
             name: 'dob',
-            placeholder: 'DOB - MM/DD/YYYY'
+            placeholder: 'DOB - DD-MM-CCYY'
           },
           {
             name: 'NINO',
@@ -232,17 +232,17 @@
       /****************/
 
       AU: {
-        appName: 'MyDollary-doos',
+        appName: 'MyMoney-AU',
         label: 'Australia (English)',
         description: 'Australia',
         code: 'AU',
         ISOCode: 'AU',
         persons: [
           ['', '', '', '', '', '', '', '', '', ''],
-          ['Sarah', 'Jamison', '02/01/1976', '165-23-9877', '17123 Peachtree Court', 'Boston', 'MA', '02196', '617-536-4100', 'sarahlynnjamison0276@yahoo.com'],
-          ['Lizzy', 'Grant', '04/04/1983', '512-43-8888', '25301 3rd Avenue APT 184', 'San Francisco', 'CA', '94107', '312-567-8500', 'lizzy_grant@gmail.com'],
-          ['Elizabeth', 'Grant', '07/04/1983', '512-43-8888', '7146 S Apostle Avenue #8160', 'Chicago', 'IL', '60652', '312-567-8500', 'elizabethgrant83@yahoo.com'],
-          ['Jeremy', 'Vasquez', '09/18/1999', '754-63-2199', '9715 Rock Maple Street', 'Austin', 'TX', '78759', '512-474-5171', 'jeremyv_007@gmail.com'],
+		  ['Sarah','Jamison','02-01-1976','165-23-9877','16521 Morris St, Unit 123','Melborn','VIC','3205','+61 4 7536 4100','sarahlynnjamison0276@yahoo.com'],
+		  ['Lizzy','Grant','09-04-1973','512-43-8888','1251 Miller St, Unit 123','North Sydney','NSW','2058','+61 4 2567 8500','lizzy_grant@gmail.com'],
+		  ['Elizabeth','Grant','09-04-1973','512-43-8888','53 Alma Rd, Unit 5a','St Kilda','VIC','3182','+61 4 2567 8500','elizabethgrant83@yahoo.com'],
+		  ['Paul','Johnson','09-18-1999','754-631-2199','9715 Rock Maple Street','Melborn','VIC','3201','+1 512-474-5171','PaulA_007@gmail.com'],
         ],
         text: {
           chooseScenario: 'Select scenario',
@@ -251,7 +251,7 @@
         },
         scenarios: {
           typical: {
-            label: 'Regular Bloke', title: 'Typical applicant: multiple services but no friction',
+            label: 'Typical Applicant', title: 'Typical applicant: multiple services but no friction',
             description: 'CrossCore strategies can be easily configured to conditionally include certain services based on risk while skipping others.',
             person: 1,
           },
@@ -290,8 +290,8 @@
             placeholder: 'DOB - MM/DD/YYYY'
           },
           {
-            name: 'ssn',
-            placeholder: 'SSN'
+            name: 'TFN',
+            placeholder: 'TFN'
           },
           {
             name: 'address',
@@ -306,8 +306,8 @@
             placeholder: 'State'
           },
           {
-            name: 'zip',
-            placeholder: 'ZIP Code'
+            name: 'postalcode',
+            placeholder: 'Postal Code'
           },
           {
             name: 'phone',
@@ -325,17 +325,110 @@
       /****************/
 
       IND: {
-        appName: 'MyCurry',
+        appName: 'MyMoney-IND',
         label: 'India (Hindu)',
         description: 'India',
         code: 'IND',
         ISOCode: 'IN',
         persons: [
           ['', '', '', '', '', '', '', '', '', ''],
-          ['Singh', 'Patel', '02/01/1976', '165-23-9877', '17123 Peachtree Court', 'Boston', 'MA', '02196', '617-536-4100', 'sarahlynnjamison0276@yahoo.com'],
-          ['Lizzy', 'Grant', '04/04/1983', '512-43-8888', '25301 3rd Avenue APT 184', 'San Francisco', 'CA', '94107', '312-567-8500', 'lizzy_grant@gmail.com'],
-          ['Elizabeth', 'Grant', '07/04/1983', '512-43-8888', '7146 S Apostle Avenue #8160', 'Chicago', 'IL', '60652', '312-567-8500', 'elizabethgrant83@yahoo.com'],
-          ['Jeremy', 'Vasquez', '09/18/1999', '754-63-2199', '9715 Rock Maple Street', 'Austin', 'TX', '78759', '512-474-5171', 'jeremyv_007@gmail.com'],
+		  ['Nikitha','Mody','02-01-1976','9423 3484 9843','65, Shop, Sv Lane, Mamulpet','Bangalore','Karnataka', '560053','+91 80 76822814','NikithaMody0276@yahoo.com'],
+		  ['Pooh','Parikh','09-04-1973','9454 2354 9765','Raj Chamber Bldg, Manchubhai Road, Malad (east)', 'Mumbai','Maharashtra','400097','+91 22 28887246','Pooh_Parikh@gmail.com'],
+		  ['Pooja','Parikh','09-04-1973','9454 2354 9765','83, Qutab View Apartments, Katwaria Sarai','New Delhi','Delhi', '110016','+91 11 23436788','PoojaParikh83@yahoo.com'],
+		  ['Arun','Pillai','09-18-1999','9453 3221 9542','46, Himalaya House, Palton Road','Mumbai','Maharashtra','400001','+91 22 27892639','ArunPillai_007@gmail.com'],
+        ],
+        text: {
+          chooseScenario: 'Select scenario',
+          approved: 'Your application was approved!',
+          approvedDetails: 'Application details and instructions for accessing your new account will be emailed shortly.',
+        },
+        scenarios: {
+          typical: {
+            label: 'Typical Applicant', title: 'Typical applicant: multiple services but no friction',
+            description: 'CrossCore strategies can be easily configured to conditionally include certain services based on risk while skipping others.',
+            person: 1,
+          },
+          mismatch: {
+            label: 'Mismatched Identities', title: 'Step-up for consumer with mismatched identity data',
+            description: 'Many applicants have slight identity discrepancies that require costly manual review and application delays.',
+            person: 2,
+          },
+          machine: {
+            label: 'Machine Learning', title: 'Avoid unnecessary step-ups using CrossCore Decision Analytics',
+            description: 'CrossCore Decision Analytics machine learning models are trained on combinations of historical data to make the best possible decision and limit the need for step-ups that add friction.',
+            person: 2,
+          },
+          fraud: {
+            label: 'Identity Fraud Attempt', title: 'Step-up for identity fraud attempt',
+            description: 'CrossCore instantly identifies most synthetic and impersonation fraud applications, adding friction to the attacker and referring the case for manual review.',
+            person: 3,
+          },
+          thin: {
+            label: 'Thin-file Applicant', title: 'Passive step-up for thin-file applicant',
+            description: 'Organizations can quickly and seamlessly incorporate additional phone intelligence or identity verification sources if the applicant was not found in traditional bureau data.',
+            person: 4,
+          }
+        },
+        fields: [
+          {
+            name: 'firstName',
+            placeholder: 'First Name'
+          },
+          {
+            name: 'lastName',
+            placeholder: 'Last Name'
+          },
+          {
+            name: 'dob',
+            placeholder: 'DOB - MM/DD/YYYY'
+          },
+          {
+            name: 'UIDAI',
+            placeholder: 'UIDAI'
+          },
+          {
+            name: 'address',
+            placeholder: '123 Streetname Ave'
+          },
+          {
+            name: 'city',
+            placeholder: 'City'
+          },
+          {
+            name: 'state',
+            placeholder: 'State'
+          },
+          {
+            name: 'postcode',
+            placeholder: 'Post Code'
+          },
+          {
+            name: 'phone',
+            placeholder: 'Phone Number'
+          },
+          {
+            name: 'email',
+            placeholder: 'email@domain.com'
+          },
+        ]
+      },
+
+      /*******************/
+      /*   INDONESIA     */
+      /*******************/
+
+      IDN: {
+        appName: 'MyMoney-IDN',
+        label: 'Indonesia',
+        description: 'Indonesia',
+        code: 'IDN',
+        ISOCode: 'ID',
+        persons: [
+          ['', '', '', '', '', '', '', '', '', ''],
+		  ['Ratna Utari','Oesman','02-01-1976','22 5600 760201 1234','Ged Surveyor Indonesia Lt Basement/LB 40','Kebayoran Baru','Jakarta','12110','021 7220074','RatanOesman0276@yahoo.com'],
+		  ['Tanu','Sasmita','09-04-1973','32 2300 790201 1234','Kompl Puri Tanjung Sari 2 50', 'Medan','Sumatera Utara','20028','061 8221782','Tanu_Sasmita@gmail.com'],
+		  ['Tanudisastro Bo','Sasmita','09-04-1973','32 2300 790201 1234','Jl Darmo Permai Tmr IV/18','Surabaya','Jawa Timur', '60111','031 7340468','TanudisastroSasmita83@yahoo.com'],
+		  ['Ary Bambang','Hardja','09-18-1999','27 3400 990918 1234','Jl HR Rasuna Said Kav 2-3 Bl X-5 Menara Kadin Indonesia Bl X-5 Lt','Kuningan','Jakarta','400001','021 5274547','AryHardja_007@gmail.com'],
         ],
         text: {
           chooseScenario: 'Select scenario',
@@ -383,8 +476,8 @@
             placeholder: 'DOB - MM/DD/YYYY'
           },
           {
-            name: 'ssn',
-            placeholder: 'SSN'
+            name: 'NIK',
+            placeholder: 'NIK - pp.DDSS.ddmmyy.ssss'
           },
           {
             name: 'address',
@@ -399,8 +492,8 @@
             placeholder: 'State'
           },
           {
-            name: 'zip',
-            placeholder: 'ZIP Code'
+            name: 'postcode',
+            placeholder: 'Post Code'
           },
           {
             name: 'phone',
@@ -412,7 +505,9 @@
           },
         ]
       },
-    };
+
+
+	  };
 
 
     var cookieLocale = getCookie( 'mm_locale' );
@@ -461,7 +556,6 @@
         label: "Machine Learning",
         toggle: false
       }
-      // Charlie changed from "CrossCore Decision Analytics" to "Machine Leanring" 5/17/19
     ]
 
     /**
