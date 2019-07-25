f0983dcf8238cf59fb8e5f861ab1a820f42ffcc5
Unknown <stanekfelix@gmail.com>
2019-07-25 11:24:44 -0500

    Reformat code

--- core/app.controller.js
+++ core/app.controller.js
@@ -1,106 +1,106 @@
-(function () {
-    'use strict';
-  
-    angular
-      .module('webApp')
-      .controller('AppController', AppController);
+(function() {
+	'use strict';
 
-    AppController.$inject = [
-        '$state',
-        '$rootScope',
-        '$animate',
-        '$location',
-        '$anchorScroll',
-        'AppStateFactory'
-    ];
+	angular
+		.module( 'webApp' )
+		.controller( 'AppController', AppController );
 
-    function AppController($state, $rootScope, $animate, $location, $anchorScroll, AppStateFactory) {
-        $animate.enabled(true)
-        var vm = this;
-        vm.appState = AppStateFactory;
-        $rootScope.pageClass = 'something-new'
-        vm.pageClass = 'something-new'
-        vm.locale = AppStateFactory.locale;
-        vm.locales = AppStateFactory.locales;
+	AppController.$inject = [
+		'$state',
+		'$rootScope',
+		'$animate',
+		'$location',
+		'$anchorScroll',
+		'AppStateFactory'
+	];
 
-        /**
-         * Template Methods
-         */
-        vm.go = function(path) {
-            $state.go(path);
-        }
+	function AppController( $state, $rootScope, $animate, $location, $anchorScroll, AppStateFactory ) {
+		$animate.enabled( true );
+		var vm = this;
+		vm.appState = AppStateFactory;
+		$rootScope.pageClass = 'something-new';
+		vm.pageClass = 'something-new';
+		vm.locale = AppStateFactory.locale;
+		vm.locales = AppStateFactory.locales;
 
-        vm.selectLocale = function(locale) {
-            document.cookie = "mm_locale=" + locale.code + ";max-age=" + 180 * 24 * 3600 * 1000 + ";";
-            setTimeout( function() {
-                window.location.replace( window.appHome + '?locale=' + locale.code );
-            }, 250 )
-        }
-        vm.selectFlow = function(flow) {
-            vm.appState.currentScenario.states = angular.copy(flow.states);
-            vm.appState.currentScenario.form = angular.copy(flow.form);
-            vm.appState.currentScenario.assets = 'assets/images/' + flow.assets + '/';
-            vm.appState.currentScenario.showPassport = (flow.assets === vm.locale.code + '/person4');
-            vm.appState.currentScenario.review = flow.name;
-            vm.appState.currentScenario.appId = flow.appId;
-            vm.next();
-        }
+		/**
+		 * Template Methods
+		 */
+		vm.go = function( path ) {
+			$state.go( path );
+		};
 
-        vm.next = function() {
-            var route = (!vm.appState.currentScenario.states.length)
-                ? 'app.mobile.welcome'
-                : vm.appState.currentScenario.states.shift();
-            
-            $state.go(route);            
-        }
+		vm.selectLocale = function( locale ) {
+			document.cookie = "mm_locale=" + locale.code + ";max-age=" + 180 * 24 * 3600 * 1000 + ";";
+			setTimeout( function() {
+				window.location.replace( window.appHome + '?locale=' + locale.code );
+			}, 250 )
+		};
+		vm.selectFlow = function( flow ) {
+			vm.appState.currentScenario.states = angular.copy( flow.states );
+			vm.appState.currentScenario.form = angular.copy( flow.form );
+			vm.appState.currentScenario.assets = 'assets/images/' + flow.assets + '/';
+			vm.appState.currentScenario.showPassport = (flow.assets === vm.locale.code + '/person4');
+			vm.appState.currentScenario.review = flow.name;
+			vm.appState.currentScenario.appId = flow.appId;
+			vm.next();
+		};
 
-        vm.anchor = function(hash) {
-            $location.hash(hash);
-            $anchorScroll();
-        }
+		vm.next = function() {
+			var route = (!vm.appState.currentScenario.states.length)
+				? 'app.mobile.welcome'
+				: vm.appState.currentScenario.states.shift();
 
-        /**
-         * Custom Events
-         */
-        $rootScope.$on('$stateChangeSuccess', function() {
-            document.body.scrollTop = document.documentElement.scrollTop = 0;
-        });
+			$state.go( route );
+		};
 
-        vm.getCookie = function( cname ) {
-            var name = cname + "=";
-            var decodedCookie = decodeURIComponent( document.cookie );
-            var ca = decodedCookie.split( ';' );
-            for ( var i = 0; i < ca.length; i++ ) {
-                var c = ca[i];
-                while ( c.charAt( 0 ) == ' ' ) {
-                    c = c.substring( 1 );
-                }
-                if ( c.indexOf( name ) == 0 ) {
-                    return c.substring( name.length, c.length );
-                }
-            }
-            return "";
-        };
-        vm.showDescriptions = false;
-        vm.isDescriptionsEnabled = function(){
-            if ( vm.getCookie( 'mm_descriptions' ) == 'true' ) {
-                vm.showDescriptions = true;
-                document.getElementById( "c-toggle" ).checked = true;
-            }
-            return true;
-        };
+		vm.anchor = function( hash ) {
+			$location.hash( hash );
+			$anchorScroll();
+		};
 
-        vm.toggleDescriptions = function() {
-            var checkbox = document.getElementById( "c-toggle" ).checked;
-            if ( checkbox ) {
-                vm.showDescriptions = true;
-                document.cookie = "mm_descriptions=true;max-age=" + 180 * 24 * 3600 * 1000 + ";";
-            } else {
-                vm.showDescriptions = false;
-                document.cookie = "mm_descriptions=false;max-age=" + 180 * 24 * 3600 * 1000 + ";";
-            }
-        }
+		/**
+		 * Custom Events
+		 */
+		$rootScope.$on( '$stateChangeSuccess', function() {
+			document.body.scrollTop = document.documentElement.scrollTop = 0;
+		} );
 
-    }
+		vm.getCookie = function( cname ) {
+			var name = cname + "=";
+			var decodedCookie = decodeURIComponent( document.cookie );
+			var ca = decodedCookie.split( ';' );
+			for ( var i = 0; i < ca.length; i++ ) {
+				var c = ca[i];
+				while ( c.charAt( 0 ) == ' ' ) {
+					c = c.substring( 1 );
+				}
+				if ( c.indexOf( name ) == 0 ) {
+					return c.substring( name.length, c.length );
+				}
+			}
+			return "";
+		};
+		vm.showDescriptions = false;
+		vm.isDescriptionsEnabled = function() {
+			if ( vm.getCookie( 'mm_descriptions' ) == 'true' ) {
+				vm.showDescriptions = true;
+				document.getElementById( "c-toggle" ).checked = true;
+			}
+			return true;
+		};
+
+		vm.toggleDescriptions = function() {
+			var checkbox = document.getElementById( "c-toggle" ).checked;
+			if ( checkbox ) {
+				vm.showDescriptions = true;
+				document.cookie = "mm_descriptions=true;max-age=" + 180 * 24 * 3600 * 1000 + ";";
+			} else {
+				vm.showDescriptions = false;
+				document.cookie = "mm_descriptions=false;max-age=" + 180 * 24 * 3600 * 1000 + ";";
+			}
+		}
+
+	}
 })();
   
--- core/app.state.factory.js
+++ core/app.state.factory.js
@@ -1,203 +1,203 @@
-(function () {
-  'use strict';
+(function() {
+	'use strict';
 
-  function getCookie( cname ) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent( document.cookie );
-    var ca = decodedCookie.split( ';' );
-    for ( var i = 0; i < ca.length; i++ ) {
-      var c = ca[i];
-      while ( c.charAt( 0 ) == ' ' ) {
-        c = c.substring( 1 );
-      }
-      if ( c.indexOf( name ) == 0 ) {
-        return c.substring( name.length, c.length );
-      }
-    }
-    return "";
-  }
+	function getCookie( cname ) {
+		var name = cname + "=";
+		var decodedCookie = decodeURIComponent( document.cookie );
+		var ca = decodedCookie.split( ';' );
+		for ( var i = 0; i < ca.length; i++ ) {
+			var c = ca[i];
+			while ( c.charAt( 0 ) == ' ' ) {
+				c = c.substring( 1 );
+			}
+			if ( c.indexOf( name ) == 0 ) {
+				return c.substring( name.length, c.length );
+			}
+		}
+		return "";
+	}
 
-  angular
-    .module('webApp')
-    .factory('AppStateFactory', AppStateFactory);
+	angular
+		.module( 'webApp' )
+		.factory( 'AppStateFactory', AppStateFactory );
 
-  AppStateFactory.$inject = ['$http'];
+	AppStateFactory.$inject = ['$http'];
 
-  /* @ngInject */
-  function AppStateFactory ($http) {
-    var vm = this;
+	/* @ngInject */
+	function AppStateFactory( $http ) {
+		var vm = this;
 
-    function generateContactForm( localeCode, person ) {
-      var form = [];
-      vm.locales[localeCode].fields.forEach( function( field, index ) {
-        form.push( {
-          name: field.name,
-          placeholder: field.placeholder,
-          value: vm.locales[localeCode].persons[person][index],
-        } );
-      } );
-      return form;
-    }
+		function generateContactForm( localeCode, person ) {
+			var form = [];
+			vm.locales[localeCode].fields.forEach( function( field, index ) {
+				form.push( {
+					name: field.name,
+					placeholder: field.placeholder,
+					value: vm.locales[localeCode].persons[person][index],
+				} );
+			} );
+			return form;
+		}
 
-    // List of locales
-    vm.locales = window.locales;
+		// List of locales
+		vm.locales = window.locales;
 
-    var localeSelector;
+		var localeSelector;
 
-    if ( window.locale ) {
-      localeSelector = window.locale;
-    } else {
-      localeSelector = getCookie( 'mm_locale' );
-    }
+		if ( window.locale ) {
+			localeSelector = window.locale;
+		} else {
+			localeSelector = getCookie( 'mm_locale' );
+		}
 
-    if ( vm.locales[localeSelector] ) {
-      vm.locale = vm.locales[localeSelector];
-    } else {
-      vm.locale = vm.locales.US;
-    }
+		if ( vm.locales[localeSelector] ) {
+			vm.locale = vm.locales[localeSelector];
+		} else {
+			vm.locale = vm.locales.US;
+		}
 
-    document.querySelector( 'body' ).classList.add( "locale-" + vm.locale.code );
-    document.querySelector( '.c-header__logotext' ).innerHTML = vm.locale.appName;
-    if ( vm.locale.ISOCode ) {
-      let flag = document.querySelector( '.c-header__locale' ).appendChild( document.createElement( 'img' ) );
-      flag.classList.add('c-header__flag');
-      flag.setAttribute( 'src', 'https://www.countryflags.io/' + vm.locale.ISOCode + '/flat/32.png' );
-    }
-    vm.currentScenario = {
-      form: {},
-      states: [],
-      assets: '',
-      review: '',
-      showPassport: false
-    }
-    vm.json = {};    
-    vm.crossCoreStrategies = [
-      {
-        label: "Identity verification",
-        toggle: true
-      },
-      {
-        label: "Risk Engine ",
-        toggle: true
-      },
-      {
-        label: "Document Verification",
-        toggle: true
-      },
-      {
-        label: "Phone Intelligence",
-        toggle: true
-      },
-      {
-        label: "Email Risk Scoring",
-        toggle: true
-      },
-      {
-        label: "Machine Learning",
-        toggle: false
-      }
-    ]
+		document.querySelector( 'body' ).classList.add( "locale-" + vm.locale.code );
+		document.querySelector( '.c-header__logotext' ).innerHTML = vm.locale.appName;
+		if ( vm.locale.ISOCode ) {
+			let flag = document.querySelector( '.c-header__locale' ).appendChild( document.createElement( 'img' ) );
+			flag.classList.add( 'c-header__flag' );
+			flag.setAttribute( 'src', 'https://www.countryflags.io/' + vm.locale.ISOCode + '/flat/32.png' );
+		}
+		vm.currentScenario = {
+			form: {},
+			states: [],
+			assets: '',
+			review: '',
+			showPassport: false
+		};
+		vm.json = {};
+		vm.crossCoreStrategies = [
+			{
+				label: "Identity verification",
+				toggle: true
+			},
+			{
+				label: "Risk Engine ",
+				toggle: true
+			},
+			{
+				label: "Document Verification",
+				toggle: true
+			},
+			{
+				label: "Phone Intelligence",
+				toggle: true
+			},
+			{
+				label: "Email Risk Scoring",
+				toggle: true
+			},
+			{
+				label: "Machine Learning",
+				toggle: false
+			}
+		];
 
-    /**
-     * Init
-     */
+		/**
+		 * Init
+		 */
 
-    /*
-    function getJson(mockData) {
-      $http.get('./assets/json/' + mockData + '.json').then(function(data) {
-        vm.json[mockData] = JSON.stringify(data, null, 3);
-      })
-    }
-    ['request', 'response'].forEach(getJson);
-    */
+		/*
+		function getJson(mockData) {
+		  $http.get('./assets/json/' + mockData + '.json').then(function(data) {
+			vm.json[mockData] = JSON.stringify(data, null, 3);
+		  })
+		}
+		['request', 'response'].forEach(getJson);
+		*/
 
-    /**
-     * Scripted Events
-     */
-    vm.scenarios = [
-      {
-        name: 'typical',
-        appId: 'CC_DEMO_' + vm.locale.proposition + '_79_' + vm.locale.code,
-        title: vm.locale.scenarios.typical.title,
-        label: vm.locale.scenarios.typical.label,
-        description: vm.locale.scenarios.typical.description,
-        form: generateContactForm( vm.locale.code, vm.locale.scenarios.typical.person ),
+		/**
+		 * Scripted Events
+		 */
+		vm.scenarios = [
+			{
+				name: 'typical',
+				appId: 'CC_DEMO_' + vm.locale.proposition + '_79_' + vm.locale.code,
+				title: vm.locale.scenarios.typical.title,
+				label: vm.locale.scenarios.typical.label,
+				description: vm.locale.scenarios.typical.description,
+				form: generateContactForm( vm.locale.code, vm.locale.scenarios.typical.person ),
 
-        // Person 1 does not have assets; do not use in scenarios other than "typical"
-        assets: '' + vm.locale.scenarios.typical.person,
-        states: [
-          'app.mobile.input',
-          'app.mobile.approved'
-        ]
-      },
-      {
-        name: 'mismatch',
-        appId: 'CC_DEMO_' + vm.locale.proposition + '_80_' + vm.locale.code,
-        title: vm.locale.scenarios.mismatch.title,
-        label: vm.locale.scenarios.mismatch.label,
-        description: vm.locale.scenarios.mismatch.description,
-        form: generateContactForm( vm.locale.code, vm.locale.scenarios.mismatch.person ),
-        assets: vm.locale.code + '/person' + vm.locale.scenarios.mismatch.person,
-        states: [
-          'app.mobile.input',
-          'app.mobile.input.needmore',
-          'app.mobile.mitek.front',
-          'app.mobile.mitek.back',			  
-          'app.mobile.mitek.selfie',
-          'app.mobile.approved'
-        ]
-      },
+				// Person 1 does not have assets; do not use in scenarios other than "typical"
+				assets: '' + vm.locale.scenarios.typical.person,
+				states: [
+					'app.mobile.input',
+					'app.mobile.approved'
+				]
+			},
+			{
+				name: 'mismatch',
+				appId: 'CC_DEMO_' + vm.locale.proposition + '_80_' + vm.locale.code,
+				title: vm.locale.scenarios.mismatch.title,
+				label: vm.locale.scenarios.mismatch.label,
+				description: vm.locale.scenarios.mismatch.description,
+				form: generateContactForm( vm.locale.code, vm.locale.scenarios.mismatch.person ),
+				assets: vm.locale.code + '/person' + vm.locale.scenarios.mismatch.person,
+				states: [
+					'app.mobile.input',
+					'app.mobile.input.needmore',
+					'app.mobile.mitek.front',
+					'app.mobile.mitek.back',
+					'app.mobile.mitek.selfie',
+					'app.mobile.approved'
+				]
+			},
 
-      {
-        name: 'machine',
-        appId: 'CC_DEMO_' + vm.locale.proposition + '_81_' + vm.locale.code,
-        title: vm.locale.scenarios.machine.title,
-        label: vm.locale.scenarios.machine.label,
-        description: vm.locale.scenarios.machine.description,
-        form: generateContactForm( vm.locale.code, vm.locale.scenarios.machine.person ),
-        assets: 'person' + vm.locale.scenarios.machine.person,
-        states: [           
-          'app.mobile.toggles',
-          'app.mobile.input',
-          'app.mobile.approved'
-         ]
-      },
-      {
-        name: 'fraud',
-        appId: 'CC_DEMO_' + vm.locale.proposition + '_76_' + vm.locale.code,
-        title: vm.locale.scenarios.fraud.title,
-        label: vm.locale.scenarios.fraud.label,
-        description: vm.locale.scenarios.fraud.description,
-        form: generateContactForm( vm.locale.code, vm.locale.scenarios.fraud.person ),
-        assets: vm.locale.code + '/person' + vm.locale.scenarios.fraud.person,
-        states: [
-          'app.mobile.input',
-          'app.mobile.input.needmore',
-          'app.mobile.mitek.front',
-          'app.mobile.mitek.back',			  
-          'app.mobile.mitek.selfie',
-          'app.mobile.input.phonemore'
-        ]
-      },
-      {
-        name: 'thin',
-        appId: 'CC_DEMO_' + vm.locale.proposition + '_82_' + vm.locale.code,
-        title: vm.locale.scenarios.thin.title,
-        label: vm.locale.scenarios.thin.label,
-        description: vm.locale.scenarios.thin.description,
-        form: generateContactForm( vm.locale.code, vm.locale.scenarios.thin.person ),
-        assets: vm.locale.code + '/person' + vm.locale.scenarios.thin.person,
-        /*          'app.mobile.mitek.back',		  */
-		states: [
-          'app.mobile.input.needmore',
-          'app.mobile.mitek.front',
-		  'app.mobile.mitek.selfie',
-          'app.mobile.input',
-          'app.mobile.approved'
-        ]
-      }
-    ]
+			{
+				name: 'machine',
+				appId: 'CC_DEMO_' + vm.locale.proposition + '_81_' + vm.locale.code,
+				title: vm.locale.scenarios.machine.title,
+				label: vm.locale.scenarios.machine.label,
+				description: vm.locale.scenarios.machine.description,
+				form: generateContactForm( vm.locale.code, vm.locale.scenarios.machine.person ),
+				assets: 'person' + vm.locale.scenarios.machine.person,
+				states: [
+					'app.mobile.toggles',
+					'app.mobile.input',
+					'app.mobile.approved'
+				]
+			},
+			{
+				name: 'fraud',
+				appId: 'CC_DEMO_' + vm.locale.proposition + '_76_' + vm.locale.code,
+				title: vm.locale.scenarios.fraud.title,
+				label: vm.locale.scenarios.fraud.label,
+				description: vm.locale.scenarios.fraud.description,
+				form: generateContactForm( vm.locale.code, vm.locale.scenarios.fraud.person ),
+				assets: vm.locale.code + '/person' + vm.locale.scenarios.fraud.person,
+				states: [
+					'app.mobile.input',
+					'app.mobile.input.needmore',
+					'app.mobile.mitek.front',
+					'app.mobile.mitek.back',
+					'app.mobile.mitek.selfie',
+					'app.mobile.input.phonemore'
+				]
+			},
+			{
+				name: 'thin',
+				appId: 'CC_DEMO_' + vm.locale.proposition + '_82_' + vm.locale.code,
+				title: vm.locale.scenarios.thin.title,
+				label: vm.locale.scenarios.thin.label,
+				description: vm.locale.scenarios.thin.description,
+				form: generateContactForm( vm.locale.code, vm.locale.scenarios.thin.person ),
+				assets: vm.locale.code + '/person' + vm.locale.scenarios.thin.person,
+				/*          'app.mobile.mitek.back',		  */
+				states: [
+					'app.mobile.input.needmore',
+					'app.mobile.mitek.front',
+					'app.mobile.mitek.selfie',
+					'app.mobile.input',
+					'app.mobile.approved'
+				]
+			}
+		];
 
-    return vm;
-  }  
+		return vm;
+	}
 })();
