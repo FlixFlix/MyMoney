18fb05034b7e41d4ee4462db897cd4795b1f5c10
Unknown <stanekfelix@gmail.com>
2020-07-22 02:26:36 -0500

    Refactored form generator function for modularity

--- core/app.state.factory.js
+++ core/app.state.factory.js
@@ -11,18 +11,6 @@
 	function AppStateFactory( $http ) {
 		var vm = this;
 
-		function generateContactForm( person ) {
-			var form = [];
-			vm.locales[vm.locale.code].fields.forEach( function( field, index ) {
-				form.push( {
-					name: field.name,
-					placeholder: field.placeholder,
-					value: vm.locales[vm.locale.code].persons[person][index],
-				} );
-			} );
-			return form;
-		}
-
 		// List of locales
 		vm.locales = window.locales;
 		const localeList = Object.keys( vm.locales );
@@ -116,28 +104,69 @@
 		/**
 		 * Scripted Events
 		 */
+
+		// Scenario constructor
 		vm.scenario = {
 			name: '',
 			get appId() {
 				return 'CC_DEMO_' + vm.locale.proposition + '_' + vm.locale[this.name] + '_' + vm.locale.code
 			},
 			get reviewUrl() {
-				if ( vm.locale.useMockupFn )
-					return encodeURI(FRAUDNET_MOCKUP_URL_BASE + vm.locale.code + '/fn-' + this.name + '.html'
+				if ( vm.locale.useMockupFN )
+					return encodeURI( FRAUDNET_MOCKUP_URL_BASE + vm.locale.code + '/fn-' + this.name + '.html'
 						+ '?locale=' + vm.locale.code
 						+ '&scenario=' + this.name
-						+ '&title=' + __( 'FraudNet Demo' ) + ' (' + vm.locale.code + ') - ' + this.scenarioName + ' - ' + this.toolTip);
+						+ '&title=' + __( 'FraudNet Demo' ) + ' (' + vm.locale.code + ') - ' + this.scenarioName + ' - ' + this.toolTip );
 				else if ( vm.isInvision && vm.locale.invision ) return INVISION_DEMO_URL_BASE + vm.locale.invision[this.name];
 				else return FRAUDNET_DEMO_URL_BASE + this.appId;
-			}
+			},
+			get form() {
+				return generateForm( vm.locale.fields, vm.locale.persons[vm.locale.scenarios[this.name].person] );
+			},
+			get form2() {
+				if ( vm.locale.hasGDC ) return generateForm( vm.locale.scenarios.gdc.address2_fields, vm.locale.scenarios.gdc.address2_values );
+				else return [];
+			},
+			get hasGDC() {
+				return this.name === 'gdc'
+			},
 		};
+
+		function generateForm( fields, values ) {
+			var form = [];
+			// console.log( 'Generating form for person:', values );
+			// console.log( 'Generating with fields:', fields );
+			fields.forEach( function( field, index ) {
+				form.push( {
+					name: field.name,
+					placeholder: field.placeholder,
+					value: values[index],
+				} );
+			} );
+			return form;
+		}
+
+		function generateContactForm_old( person, fields = 'fields' ) {
+			var form = [], values;
+			console.log( 'Generating form for person:', person );
+			console.log( 'Generating with fields:', fields );
+			values = typeof person !== 'number' ? person : locales[vm.locale.code].persons[person];
+			vm.locales[vm.locale.code][fields].forEach( function( field, index ) {
+				form.push( {
+					name: field.name,
+					placeholder: field.placeholder,
+					value: values[index],
+				} );
+			} );
+			return form;
+		}
+
 		vm.scenarios = [
 			Object.assign( Object.create( vm.scenario ), {
 				name: 'typical',
 				scenarioName: __( 'Typical Applicant' ),
 				toolTip: __( 'Typical applicant: multiple services but no friction' ),
 				description: __( 'Strategies can be easily configured to conditionally include certain services based on risk while skipping others.' ),
-				form: generateContactForm( vm.locale.scenarios.typical.person ),
 
 				// Person 1 does not have assets; do not use in scenarios other than "typical"
 				assets: '' + vm.locale.scenarios.typical.person,
@@ -151,7 +180,6 @@
 				scenarioName: __( 'Mismatched Identities' ),
 				toolTip: __( 'Step-up for consumer with mismatched identity data' ),
 				description: __( 'Machine learning models are trained on combinations of historical data to make the best possible decision and limit the need for step-ups that add friction.' ),
-				form: generateContactForm( vm.locale.scenarios.mismatch.person ),
 				assets: vm.locale.code + '/person' + vm.locale.scenarios.mismatch.person,
 
 				// Different set of steps based on locale parameter
@@ -173,13 +201,11 @@
 				]
 
 			} ),
-
 			Object.assign( Object.create( vm.scenario ), {
 				name: 'machine',
 				scenarioName: __( 'Machine Learning' ),
 				toolTip: __( 'Avoid unnecessary step-ups using Decision Analytics' ),
 				description: __( 'Machine learning models are trained on combinations of historical data to make the best possible decision and limit the need for step-ups that add friction.' ),
-				form: generateContactForm( vm.locale.scenarios.machine.person ),
 				assets: 'person' + vm.locale.scenarios.machine.person,
 				states: [
 					'app.mobile.toggles',
@@ -192,7 +218,6 @@
 				scenarioName: __( 'Identity Fraud Attempt' ),
 				toolTip: __( 'Step-up for identity fraud attempt' ),
 				description: __( 'Instantly identify most synthetic and impersonation fraud applications, adding friction to the attacker and referring the case for manual review.' ),
-				form: generateContactForm( vm.locale.scenarios.fraud.person ),
 				assets: vm.locale.code + '/person' + vm.locale.scenarios.fraud.person,
 
 				// Different set of steps based on locale parameter
@@ -217,7 +242,6 @@
 				scenarioName: __( 'Thin-file Applicant' ),
 				toolTip: __( 'Passive step-up for thin-file applicant' ),
 				description: __( 'Organizations can quickly and seamlessly incorporate additional phone intelligence or identity verification sources if the applicant was not found in traditional bureau data.' ),
-				form: generateContactForm( vm.locale.scenarios.thin.person ),
 				assets: vm.locale.code + '/person' + vm.locale.scenarios.thin.person,
 
 				/* There are two variables, DrvIDBackside and GovIDBackside, that way some can show both sides and others not - TT 7/25/19 */
